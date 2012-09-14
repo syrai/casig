155824537b2c3849ccdd89c50dd5c00342d6aa26
syrai <syrai@yahoo.fr>
2012-09-14 17:26:55 +0200

    Nouveau

--- abonne/ajax_abonne.php
+++ abonne/ajax_abonne.php
@@ -1,6 +1,7 @@
 <?php
 //Include de connexion
 include("../connexion/connex.inc.php");
+// Informations sur les abonnés
 if(isset($_POST['action']) && !empty($_POST['action']) && $_POST['action']=="info_abonne")
 	{
 		$idcom=connex("SIA","myparam");
