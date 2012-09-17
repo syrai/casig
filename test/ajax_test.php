c17ca57f8d698bc0ef67fb98f8d97522771fce71
syrai <syrai@yahoo.fr>
2012-09-17 18:21:11 +0300

    Update test/ajax_test.php

--- test/ajax_test.php
+++ test/ajax_test.php
@@ -1,18 +1,18 @@
 <?php
 if(isset($_FILES) && is_array($_FILES)) {
 // Nombre de fichiers envoyés
-$nb = count($_FILES['mesfichiers']['name']);
+$nb = count($_FILES['f']['name']);
 // Chemin destination (répertoire courant + /upload/)
 $dir = realpath('.').'/upload/';
 if($nb>0) {
 for($i=0;$i<$nb;$i++) {
-echo '<p>Fichier : '.$_FILES['mesfichiers']['name'][$i];
-echo '<br>Taille : '.$_FILES['mesfichiers']['size'][$i];
-echo '<br>Type : '.$_FILES['mesfichiers']['type'][$i];
+echo '<p>Fichier : '.$_FILES['f']['name'][$i];
+echo '<br>Taille : '.$_FILES['f']['size'][$i];
+echo '<br>Type : '.$_FILES['f']['type'][$i];
 // Copie depuis le répertoire temporaire
 $copie =
-move_uploaded_file($_FILES['mesfichiers']['tmp_name'][$i],
-$uploaddir.$_FILES['mesfichiers']['name'][$i]);
+move_uploaded_file($_FILES['f']['tmp_name'][$i],
+$uploaddir.$_FILES['f']['name'][$i]);
 if($copie) echo '<br><b>Fichier copié</b></p>'; else echo
 '<br><b>Erreur de copie</b></p>';
 }
