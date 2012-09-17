a85c6cd8d31509a723162078749f5c75e2472d5a
syrai <syrai@yahoo.fr>
2012-09-17 14:46:26 +0300

    Passage à la version 1.2.0.RC1 de jquerymobile

--- connexion/version_jq.php
+++ connexion/version_jq.php
@@ -1,5 +1,4 @@
-
 <link rel="shortcut icon" href="../img/mobile/favicon.png" />
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0-beta.1/jquery.mobile-1.2.0-beta.1.min.css" />
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0-rc.1/jquery.mobile-1.2.0-rc.1.min.css" />
 <script src="http://code.jquery.com/jquery-1.8.1.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.2.0-beta.1/jquery.mobile-1.2.0-beta.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.2.0-rc.1/jquery.mobile-1.2.0-rc.1.min.js"></script>
