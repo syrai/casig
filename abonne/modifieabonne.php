8c4d4b73c6b75d23ed21b4ff236e695107cb01f0
syrai <syrai@yahoo.fr>
2012-09-24 15:37:04 +0300

    Update abonne/modifieabonne.php

--- abonne/modifieabonne.php
+++ abonne/modifieabonne.php
@@ -37,11 +37,13 @@
 				
 					buffer='<h3>Modifier ' + row[1] + '</h3>';
 					buffer=buffer + '<table class="table"><tr>';
-					buffer=buffer + '<th>Nom exploitation :</th><td><input type="text" name="username" id="username" value="' + row[1] + '" /></td></tr>';
-					buffer=buffer + '<tr><th>Nom contact :</th><td><input type="text" name="nom" id="nom" value="' + row[2] + '" /></td></tr>';
-					buffer=buffer + '<tr><th>Adresse :</th><td><input type="text" name="adresse" id="adresse" value="' + row[5] + '" /></td></tr>';
+					buffer=buffer + '<th>Nom exploitation :</th><td><input type="text" name="username" id="username" value="' + row[1] + '" /></td><td><a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_rs()"></a></td></tr>';
+					buffer=buffer + '<tr><th>Nom contact :</th><td><input type="text" name="nom" id="nom" value="' + row[2] + '" /></td><td><a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_nom()"></a></td></tr>';
+					buffer=buffer + '<tr><th>Adresse :</th><td><input type="text" name="adresse" id="adresse" value="' + row[5] + '" /></td><td><a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_adresse()"></a></td></tr>';
 					buffer=buffer + '<tr><th>Tél :</th><td><input type="tel" name="tel" id="tel" value="' + row[3] + '" /></td><td><a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_tel()"></a></td></tr>';
 					buffer=buffer + '<tr><th>Mail:</th><td><input type="email" name="email" id="email" value="' + row[4] + '" /></td><td><a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_mail()"></a></td></tr>';
+					buffer=buffer + '<tr><th>Tél :</th><td><input type="text" name="identifiant" id="identifiant" value="" /></td><td><a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_login()"></a></td></tr>';
+					buffer=buffer + '<tr><th>Mail:</th><td><input type="text" name="passe" id="passe" value="" /></td><td><a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_passe()"></a></td></tr>';
 					buffer=buffer + '</table>';
 					buffer=buffer + '<a href="" id="d" data-role="button"  data-inline="true" data-theme="e">Mettre à jour</a>';
 				
@@ -80,6 +82,79 @@
 			},
 			success : function(data,text){	
 				alert('Email mis à jour !');
+			}
+		})
+	}
+	// Changer la raison social
+	function modif_rs(idexploitation){
+		$.ajax({
+			type: 'POST',
+			url: 'ajax_abonne.php',
+			data: {
+				action: 'modif_rs',
+				raisonsocial : $('#username').val()
+				idexploitation : localStorage.idexploitation
+			},
+			success : function(data,text){	
+				alert('Raison social mis à jour !');
+			}
+		})
+	}
+	// Changer le nom
+	function modif_nom(idexploitation){
+		$.ajax({
+			type: 'POST',
+			url: 'ajax_abonne.php',
+			data: {
+				action: 'modif_nom',
+				nom : $('#nom').val(),
+				idexploitation : localStorage.idexploitation
+			},
+			success : function(data,text){	
+				alert('Nom mis à jour !');
+			}
+		})
+	}
+		// Changer le login
+	function modif_login(idexploitation){
+		$.ajax({
+			type: 'POST',
+			url: 'ajax_abonne.php',
+			data: {
+				action: 'modif_login',
+				nom : $('#identifiant').val(),
+				idexploitation : localStorage.idexploitation
+			},
+			success : function(data,text){	
+				alert('Identifiant mis à jour !');
+			}
+		})
+	}
+	function modif_passe(idexploitation){
+		$.ajax({
+			type: 'POST',
+			url: 'ajax_abonne.php',
+			data: {
+				action: 'modif_passe',
+				nom : $('#passe').val(),
+				idexploitation : localStorage.idexploitation
+			},
+			success : function(data,text){	
+				alert('Mot de passe mis à jour !');
+			}
+		})
+	}
+	function modif_adresse(idexploitation){
+		$.ajax({
+			type: 'POST',
+			url: 'ajax_abonne.php',
+			data: {
+				action: 'modif_adresse',
+				nom : $('#adresse').val(),
+				idexploitation : localStorage.idexploitation
+			},
+			success : function(data,text){	
+				alert('Adresse mise à jour !');
 			}
 		})
 	}
