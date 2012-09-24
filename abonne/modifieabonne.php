8447cb6628b269ef4d752326050a11f1dfbedc47
syrai <syrai@yahoo.fr>
2012-09-24 17:08:21 +0300

    Update abonne/modifieabonne.php

--- abonne/modifieabonne.php
+++ abonne/modifieabonne.php
@@ -37,13 +37,13 @@
 				
 					buffer='<h3>Modifier ' + row[1] + '</h3>';
 					buffer=buffer + '<ul data-role="listview" data-inset="true"';
-					buffer=buffer + '<li data-role="fieldcontain"';
+					buffer=buffer + '<li data-role="fieldcontain"'>;
 					buffer=buffer + '<label for="username">Exploitation : </label>';
 					buffer=buffer + '<input type="text" name="username" id="username" value="' + row[1] + '" />';
 					buffer=buffer + '<a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_rs()"></a>';
 					buffer=buffer + '</li>';
-					buffer=buffer + '<li data-role="fieldcontain"';
-					buffer=buffer + '<label for="nom">Exploitation : </label>';
+					buffer=buffer + '<li data-role="fieldcontain"'>;
+					buffer=buffer + '<label for="nom">Nom : </label>';
 					buffer=buffer + '<input type="text" name="nom" id="username" value="' + row[2] + '" />';
 					buffer=buffer + '<a href="" data-role="button" data-icon="refresh" data-theme="f" data-iconpos="notext" onclick="modif_nom()"></a>';
 					buffer=buffer + '</li>';
