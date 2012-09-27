fb1991427bfc62a422e6a5d20bfd19b1f2fb2ca0
syrai <syrai@yahoo.fr>
2012-09-27 18:41:50 +0300

    Update connexion/version_jq.php

--- connexion/version_jq.php
+++ connexion/version_jq.php
@@ -2,4 +2,3 @@
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0-rc.2/jquery.mobile-1.2.0-rc.2.min.css" />
 <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.2.0-rc.2/jquery.mobile-1.2.0-rc.2.min.js"></script>
-
