079b9367d773d0bc1812e8cdd6baa14771ce63e6
syrai <syrai@yahoo.fr>
2012-09-27 12:46:39 +0300

    Update abonne/bilan.php

--- abonne/bilan.php
+++ abonne/bilan.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1"> 
@@ -42,5 +42,8 @@ function afficher_bilan()
   
 }
 </script>
+	<?php
+	include_once("../inc_footer.php/footer_cda.inc.php");
+?>
 </body>
 </html>
