f6af13c90023ff7c3f4516ab0429a54bfa95ea04
syrai <syrai@yahoo.fr>
2012-09-27 12:51:26 +0300

    Update abonne/ajax_a.php

--- abonne/ajax_a.php
+++ abonne/ajax_a.php
@@ -1,3 +1,24 @@
 <?php
+include("../connexion/connex.inc.php");
 
+if(isset($_POST['action']) && !empty($_POST['action']) && $_POST['action']=="afficher_le_bilan")
+  {
+	$idcom=connex("SIA","myparam");
+	$requete="select libelle,count(idexploitation) as n FROM tfacturation JOIN ttypeabonnement tt USING (idtypeabonnement)  ";
+	$requete.=" WHERE campagne=2013 GROUP BY libelle ORDER BY n desc;"
+	echo $requete;
+	$result=pg_query($idcom,$requete);
+	if(pg_num_rows($result)>0) {
+			$myarray = array();
+			while ($row = pg_fetch_row($result)) {
+  				$myarray[] = $row;
+			}
+			echo json_encode($myarray);
+		}
+		else {
+			echo json_encode(0);
+		}	
+	pg_close($idcom);
+	
+}
 ?>
