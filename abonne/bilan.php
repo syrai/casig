09b6f3200ae3b24854b2bf26139fe6c04e30e707
syrai <syrai@yahoo.fr>
2012-09-27 11:25:25 +0300

    Première version simple du bilan

--- abonne/bilan.php
+++ abonne/bilan.php
@@ -1,3 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<title>Bilan</title>
+  <?php
+  include_once("../connexion/version_jq.php");
+	?>
+</head>
+<body>
+<div data-role="header" data-theme="e" data-position="fixed">
+<h1>Bilan</h1>
+<a href="../h.php" rel="external" data-icon="home" data-iconpos="notext" data-transition="fade" >Home</a>
+</div>
+<div id="liste">
 
-?>
+</div>
+<script typep="text/javascript">
+function afficher_bilan()
+</script>{
+  $.ajax({
+  type: 'POST',
+  url: 'ajax_abonne.php',
+  data: {
+    action: 'afficher_le_bilan'
+  },
+  success : function(data){
+    buffer='<ul data-role="listview" id="ul_bilan" data-theme="f">';
+    var obj = jQuery.parseJSON(data);
+    for(i=0;i<obj.length;i++){
+    var tmp=obj[i];
+      buffer=buffer + '<li>' + tmp[0] + '<span class="ui-li-count">' + tmp[1] + '</span></li>';
+          }
+      buffer=buffer + '</ul>';
+      $('#liste').html(buffer1);
+  		$('#liste').trigger('create');	
+  }
+  })
+  
+}
+</body>
+</html>
