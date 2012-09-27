65554c3f16ea4aa0aec00040202d0bf67a2479fb
syrai <syrai@yahoo.fr>
2012-09-27 15:02:31 +0300

    Update abonne/ajax_a.php

--- abonne/ajax_a.php
+++ abonne/ajax_a.php
@@ -19,4 +19,22 @@ if(isset($_POST['action']) && !empty($_POST['action']) && $_POST['action']=="aff
 	pg_close($idcom);
 	
 }
+if(isset($_POST['action']) && !empty($_POST['action']) && $_POST['action']=="afficher_total")
+  {
+	$idcom=connex("SIA","myparam");
+	$requete="select count(idexploitation) as n FROM tfacturation JOIN ttypeabonnement tt USING (idtypeabonnement) WHERE campagne=2013 ";
+	$result=pg_query($idcom,$requete);
+	if(pg_num_rows($result)>0) {
+			$myarray = array();
+			while ($row = pg_fetch_row($result)) {
+  				$myarray[] = $row;
+			}
+			echo json_encode($myarray);
+		}
+		else {
+			echo json_encode(0);
+		}	
+	pg_close($idcom);
+
+}
 ?>
