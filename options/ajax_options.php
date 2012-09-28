2caffd55806c04f551dda3fa2143d55d0b12dda1
syrai <syrai@yahoo.fr>
2012-09-28 09:26:23 +0200

    Ok

--- options/ajax_options.php
+++ options/ajax_options.php
@@ -19,4 +19,23 @@ if(isset($_POST['action']) && !empty($_POST['action']) && $_POST['action']=="aff
 	pg_close($idcom);
 	
 }
+// Info sur la disponiblilte
+if(isset($_POST['action']) && !empty($_POST['action']) && $_POST['action']=="afficher_liste_abo")
+  {
+	$idcom=connex("SIA","myparam");
+	$requete="SELECT disponible FROM  ttypeabonnement tt  WHERE idtypeabonnement='".$_POST['idtypeabonnement']."'";
+	$result=pg_query($idcom,$requete);
+	if(pg_num_rows($result)>0) {
+			$myarray = array();
+			while ($row = pg_fetch_row($result)) {
+  				$myarray[] = $row;
+			}
+			echo json_encode($myarray);
+		}
+		else {
+			echo json_encode(0);
+		}	
+	pg_close($idcom);
+	
+}
 ?>
