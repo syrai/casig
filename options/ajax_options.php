6db4720894aefa664b7733d4328b3bb8dd4870cd
syrai <syrai@yahoo.fr>
2012-09-28 09:04:17 +0200

    Ajout du fichier de traitements des optiosn

---
+++ options/ajax_options.php
@@ -0,0 +1,22 @@
+<?php
+include("../connexion/connex.inc.php");
+// Recherche des types d'abonnement pour l'affichage dans la liste dans la page gest_param
+if(isset($_POST['action']) && !empty($_POST['action']) && $_POST['action']=="afficher_liste_abo")
+  {
+	$idcom=connex("SIA","myparam");
+	$requete="select libelle || ', ' || tarif ||  as n ,idtypeabonnement FROM  ttypeabonnement tt  ORDER BY libelle desc";
+	$result=pg_query($idcom,$requete);
+	if(pg_num_rows($result)>0) {
+			$myarray = array();
+			while ($row = pg_fetch_row($result)) {
+  				$myarray[] = $row;
+			}
+			echo json_encode($myarray);
+		}
+		else {
+			echo json_encode(0);
+		}	
+	pg_close($idcom);
+	
+}
+?>
