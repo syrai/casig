ef65e6718219333839e3e78c6370206cd60fa533
syrai <syrai@yahoo.fr>
2012-10-04 16:36:53 +0200

    Correction suite a merge

--- connexion/version_jq.php
+++ connexion/version_jq.php
@@ -1,11 +1,7 @@
 <link rel="shortcut icon" href="../img/mobile/favicon.png" />
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
 <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-<<<<<<< HEAD
 <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
-=======
-<script src="http://code.jquery.com/mobile/1.2.0-rc.2/jquery.mobile-1.2.0-rc.2.min.js"></script>	
-<!-- Fichiers pour fenetre alert -->
 <script src="../jqueryalert/jquery.alerts.js" type="text/javascript"></script>
 <link href="../jqueryalert/jquery.alerts.css" rel="stylesheet" type="text/css" media="screen" />
->>>>>>> gestion_parametrages
+
